ac40c1e1fd433eb9738d528a94d91e0fe326537f
Beardocracy <travisjbearden@gmail.com>
2020-02-14 00:38:35 +0000

    Added tests for save method, created_at, updated_at

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 ''' Module for testing the class BaseModel '''
 import unittest
+import datetime
+import time
 from models.base_model import BaseModel
 
 class TestBaseModel(unittest.TestCase):
@@ -16,8 +18,42 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(type(b1.id), str)
         self.assertRegex(b1.id,
                          r'[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}')
+        b2 = BaseModel()
+        self.assertNotEqual(b1.id, b2.id)
 
     def test_created_updated_at(self):
-        ''' Tests create and update time variables at instance creation.'''
+        ''' Tests the format of the create/update_at variables creation.'''
         b1 = BaseModel()
-        self.assertEqual(1, 1)
+        self.assertEqual(type(b1.created_at), str)
+        self.assertEqual(type(b1.updated_at), str)
+        self.assertRegex(b1.created_at,
+                         r'\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d[.]\d{6}')
+        self.assertRegex(b1.updated_at,
+                         r'\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d[.]\d{6}')
+
+    def test_save_method(self):
+        ''' Tests the save method for correct time'''
+        b1 = BaseModel()
+        temp_time = b1.updated_at
+        b1.save()
+        self.assertNotEqual(b1.updated_at, temp_time)
+        initial_time = b1.updated_at
+        time.sleep(.1)
+        b1.save()
+        later_time = b1.updated_at
+        time_passed = float(later_time[-8:]) - float(initial_time[-8:])
+        self.assertTrue(bool(time_passed > .1))
+
+    def test_created_at_time(self):
+        ''' Tests that created_at method is assigning correct time '''
+        b1 = BaseModel()
+        time.sleep(.1)
+        time_later = datetime.datetime.today().isoformat()
+        time_diff = float(time_later[-8:]) - float((b1.created_at)[-8:])
+        self.assertGreater(time_diff, .1)
+        self.assertEqual(b1.created_at[0:-8], time_later[0:-8])
+
+    def test_str_(self):
+        ''' Tests the format of __str__ method '''
+
+
