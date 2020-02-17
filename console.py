7498416eebed19fce9c9174040f1f309a65beb86
Beardocracy <travisjbearden@gmail.com>
2020-02-17 20:15:39 +0000

    Added create method

--- console.py
+++ console.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 ''' This module contains the entry point for the command interpreter '''
 import cmd
+import models
+from models.base_model import BaseModel
+
+classList = ['BaseModel']
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
@@ -18,6 +22,38 @@ class HBNBCommand(cmd.Cmd):
 
     do_EOF = do_quit
     help_EOF = help_quit
+
+    def do_create(self, s):
+        ''' Creates a new instance of the class and saves it '''
+        if s not in classList:
+            if len(s) == 0:
+                print("** class name missing **")
+            else:
+                print("** class doesn't exist **")
+        else:
+            new_object = eval("{}()".format(s))
+            models.storage.save()
+
+    def help_create(self):
+        print("Creates a new instance of the class, saves it to file.")
+        print("Ex $ create BaseModel")
+        print()
+
+    def do_all(self, s):
+        ''' Prints all instances of the class name '''
+        if s == "BaseModel":
+            print(models.storage.all())
+            '''
+            for keys in storage.__objects.keys():
+                print(storage.__objects[keys])
+            '''
+        else:
+            print("** class doesn't exist **")
+
+    def help_all(self):
+        print("Prints all instances based of the class name.")
+        print("Ex: $ all BaseModel")
+        print()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
