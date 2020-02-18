132aa155d6c2c68e68db8a34f8b491138c776058
Beardocracy <travisjbearden@gmail.com>
2020-02-18 01:07:43 +0000

    Added new classes

--- console.py
+++ console.py
@@ -4,8 +4,14 @@ import cmd
 import re
 import models
 from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.state import State
+from models.place import Place
+from models.review import Review
+from models.user import User
 
-classList = ['BaseModel']
+classList = ['BaseModel', 'User', 'City', 'State', 'Place', 'Amenity', 'Review']
 
 
 class HBNBCommand(cmd.Cmd):
