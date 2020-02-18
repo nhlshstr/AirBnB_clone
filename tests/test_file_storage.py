970c87ba03a6fa77e7535e7ddc32fc8ca13f7937
Nehal Shastri <nhlshstr@gmail.com>
2020-02-18 13:08:14 -0800

    added setup and destroy functions

--- tests/test_file_storage.py
+++ tests/test_file_storage.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python3
 ''' Module for testing the class FileStorage '''
 import unittest
+import os
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
 class TestFileStorage(unittest.TestCase):
 
-    '''
+
     def SetUp(self):
+        """Creates new file_storage when a test runs"""
+        jPath = ""
         FileStorage._FileStorage__objects = {}
-    '''
+        TestFileStorage._jPath = FileStorage._FileStorage__file_path
+        FileStorage._FileStorage__file_path = "sample.json"
+
+    def Destroy(self):
+        """Removes sample.json file"""
+        try:
+            os.remove("sample.json")
+        except:
+            pass
+        FileStorage._FileStorage__file_path = TestFileStorage.jPath
+
+
     def all(self):
         ''' Tests that the all method returns a dictionary '''
         s1 = FileStorage()
@@ -17,7 +31,7 @@ class TestFileStorage(unittest.TestCase):
         s.new(b1)
         temp = s.all()
         self.assertEqual(type(temp), dict)
-    
+
     def test_new(self):
         ''' Test that the new function adds a key:value pair to __objects '''
         s = FileStorage()
@@ -28,3 +42,5 @@ class TestFileStorage(unittest.TestCase):
         self.assertEqual(len(s.all()), 1)
         FileStorage._FileStorage__objects = {}
         s.reload()
+
+
