a3c30a4edc7731bef49a2b3c996318298208d678
Nehal Shastri <nhlshstr@gmail.com>
2020-02-18 17:10:11 -0800

    corrected syntax

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -19,60 +19,60 @@ class testCity(unittest.TestCase):
     def testCityID(self):
         """To check if city ID is of type string"""
         x1 = Place()
-        self.assertEqual(type(x1.city_id, str)
-        self.assertEqual(x1.state_id, "")
+        self.assertEqual(type(x1.city_id), str)
+        self.assertEqual(x1.city_id, "")
 
     def testUserID(self):
         """To check if user ID is of type string"""
         x1 = Place()
-        self.assertEqual(type(x1.user_id, str)
+        self.assertEqual(type(x1.user_id), str)
         self.assertEqual(x1.user_id, "")
 
     def testDescription(self):
         """To check if description is of type string"""
         x1 = Place()
-        self.assertEqual(type(x1.description, str)
+        self.assertEqual(type(x1.description), str)
         self.assertEqual(x1.description, "")
-    
+
     def testNoRooms(self):
         """To check rooms"""
         x1 = Place()
-        self.assertEqual(type(x1.number_rooms, int)
+        self.assertEqual(type(x1.number_rooms), int)
         self.assertEqual(x1.number_rooms, 0)
-    
+
     def testNoBathRooms(self):
         """To check bathrooms"""
         x1 = Place()
-        self.assertEqual(type(x1.number_bathrooms, int)
+        self.assertEqual(type(x1.number_bathrooms), int)
         self.assertEqual(x1.number_bathrooms, 0)
-    
+
     def testMaxGuests(self):
         """To check max guests"""
         x1 = Place()
-        self.assertEqual(type(x1.max_guests, int)
+        self.assertEqual(type(x1.max_guests), int)
         self.assertEqual(x1.max_guests, 0)
 
     def testNightPrice(self):
         """To check nightly price"""
         x1 = Place()
-        self.assertEqual(type(x1.price_by_night, int)
+        self.assertEqual(type(x1.price_by_night), int)
         self.assertEqual(x1.price_by_night, "")
 
 
     def testLatitude(self):
         """To check latitude"""
         x1 = Place()
-        self.assertEqual(type(x1.latitude, float)
+        self.assertEqual(type(x1.latitude), float)
         self.assertEqual(x1.latitude, 0.0)
 
     def testLongitude(self):
         """To check longitude"""
         x1 = Place()
-        self.assertEqual(type(x1.longitude, float)
+        self.assertEqual(type(x1.longitude), float)
         self.assertEqual(x1.longitude, 0.0)
 
     def testAmenityID(self):
         """To check amenityID"""
         x1 = Place()
-        self.assertEqual(type(x1.amenity_ids, list)
-        self.assertEqual(x1.amenity_ids, [])
+        self.assertEqual(type(x1.amenity_ids), list)
+        self.assertEqual(x1.amenity_ids[0], "")
